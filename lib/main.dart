3c3ff153b4892e390d9e5cd3e91a3ddce97de9f1
Eduarda Trindade <egtrindade1@gmail.com>
2021-03-16 00:51:15 -0300

    Add roll dice method with random numbers

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,39 +31,55 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () {
-                  setState(() {
-                    leftDiceNumber = 5;
-                  });
-                },
-                child: Image.asset('images/dice$leftDiceNumber.png'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Press on a dice to roll:',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+          ),
+        ),
+        SizedBox(
+          height: 16.0,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                  onPressed: () {
+                    rollDice();
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () {
-                  setState(() {
-                    rightDiceNumber = 6;
-                  });
-                },
-                child: Image.asset('images/dice$rightDiceNumber.png'),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                  onPressed: () {
+                    rollDice();
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
               ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
